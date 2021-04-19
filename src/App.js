9fd1ce018f2340ecb1ad3baa2f11e3d051ef5021
Your Name <jgbharris@hotmail.co.uk>
2021-04-19 18:33:19 +0100

    setting up component files, basic css and imports

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-
+import WeatherPage from './WeatherPage'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-    
+    <div> TEST</div>
+    <WeatherPage></WeatherPage>
     </div>
   );
 }
---
+++ src/WeatherPage.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from "react";
+import CurrentWeather from './CurrentWeather'
+import './App.css';
+
+function WeatherPage() {
+
+
+
+
+
+
+    return (
+      <div>
+      TEST
+      </div>
+    );
+  }
+  
+  export default WeatherPage;
